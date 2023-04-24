8ffed47db990497ec1d4450fd43862f7540469cc
Elaidzha1940 <eli080293@gmail.com>
2023-04-24 12:07:30 +0700

    Tabbar / Search + Fixed the Project and file names + added a New File

---  Search/ Search/ContentView.swift
+++  Search/ Search/ContentView.swift
@@ -9,20 +9,105 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+
+enum MenuType {
+    case orders, executors, cabinet, profile, addNew
+}
+
+
+
+
+struct MainView: View {
+    @EnvironmentObject var observable: Observable
+    
+    @State var is_active: MenuType = .orders
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            if !observable.showMain {
+                HelloView()
+                    .transition(.opacity)
+
+            } else if !observable.netStatus {
+                
+                InternetConnectionError()
+                
+            } else if !observable.is_validate {
+                Login()
+            } else {
+                VStack {
+                    ZStack {
+                        Color.init(red: 0.942, green: 0.958, blue: 0.958)
+                            .ignoresSafeArea()
+                        VStack {
+                            if is_active == .orders || is_active == .executors || is_active == .addNew {
+                                LogoTop()
+                            }
+                            if is_active == .orders || is_active == .executors {
+                                SearchBar()
+                            }
+                            
+                            switch is_active {
+                            case .orders:
+                                OrdersList()
+                            case .executors:
+                                ExecutorsList()
+                            case .cabinet:
+                                Cabinet()
+                            case .profile:
+                                ProfileView()
+                            case .addNew:
+                                AddNewOrder()
+                            }
+                            
+                            Spacer()
+                        }
+                    }
+                    
+                    TabMenu(is_active: $is_active)
+                        .padding(.top, 10)
+                        .cornerRadius(10)
+                        .overlay(
+                            Button {
+                                withAnimation {
+                                    is_active = .addNew
+                                }
+                            } label: {
+                                CirclePlus(is_active: $is_active)
+                                    .padding(.bottom, 50)
+                            }
+                        )
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            observable.monitor.start(queue: observable.queue)
+            observable.monitor.pathUpdateHandler = { path in
+                if path.status == .satisfied {
+                    DispatchQueue.main.async {
+                        observable.netStatus = true
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        observable.netStatus = false
+                    }
+                }
+            }
+            observable.is_validate = UserDefaults.standard.bool(forKey: "is_validate")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation(.easeOut(duration: 2)) {
+                    observable.showMain = true
+                }
+
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
+            .environmentObject(Observable())
     }
 }
---
+++  Search/ Search/InternetConnectionError.swift
@@ -0,0 +1,20 @@
+//
+//  InternetConnectionError.swift
+//   Search
+//
+//  Created by Elaidzha Shchukin on 24.04.2023.
+//
+
+import SwiftUI
+
+struct InternetConnectionError: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InternetConnectionError_Previews: PreviewProvider {
+    static var previews: some View {
+        InternetConnectionError()
+    }
+}
