e0dab8f9799092cd322646904b7f15eb19b59b8c
Elaidzha1940 <eli080293@gmail.com>
2023-04-24 12:21:04 +0700

    Error

---  Search/ Search/InternetConnectionError.swift
+++  Search/ Search/InternetConnectionError.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct InternetConnectionError: View {
+    @EnvironmentObject var observable: Observable
+    
+    
+    func tryConection() {
+        print(observable.netStatus)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("interneterror")
+                    .padding()
+                
+                Text("Проблемы с интернет подключением")
+                    .font(.custom(FontsManager.Inter.regular, size: 16))
+                    .foregroundColor(
+                        Color.init(toText: .gwork)
+                    )
+              
+                Button {
+                    tryConection()
+                } label: {
+                    Text("Попробовать ещё раз")
+                        .font(.custom(FontsManager.Inter.regular, size: 16))
+                        .frame(width: 225, height: 43)
+                        .foregroundColor(
+                            Color.init(toText: .gmain)
+                            
+                        )
+                        .cornerRadius(4)
+                        .padding()
+                }
+                
+                Button {
+                    tryConection()
+                } label: {
+                    Text("Вернуться на главную")
+                        .font(.custom(FontsManager.Inter.regular, size: 16))
+                        .frame(width: 225, height: 43)
+                        .background(
+                            Color.init(toElement: .main)
+                        )
+                        .foregroundColor(.white)
+                        .cornerRadius(4)
+                        .padding()
+                }
+            }
+            .padding()
+        
     }
 }
 
 struct InternetConnectionError_Previews: PreviewProvider {
     static var previews: some View {
-        InternetConnectionError()
+        self.devices {
+            InternetConnectionError()
+        }
     }
 }
