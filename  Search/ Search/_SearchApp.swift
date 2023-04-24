681b3366623cfa9c6bf354c97ca039682e41dac5
Elaidzha1940 <eli080293@gmail.com>
2023-04-24 11:57:59 +0700

    Tabbar / Search

---
+++  Search/ Search/_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SearchApp.swift
+//   Search
+//
+//  Created by Elaidzha Shchukin on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct _SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
